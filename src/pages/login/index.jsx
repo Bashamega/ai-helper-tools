33d882aa073f0ca7ecc2afca9741bfe6bd24ad2d
Bashamega <adambashaahmednaji@gmail.com>
2024-05-18 10:25:06 +0300

    update

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,24 @@
-import React from "react"
-import { Login } from "@/components/login"
+import { getSession } from "next-auth/react";
+import React, { useState, useEffect } from "react";
+import { authOptions } from "./api/auth/[...nextauth]";
+import SessionProvider from "@/components/SessionProvider";
+import { Login } from "@/components/login";
+
 export default function LoginPage() {
-    return(
-        <Login/>
-    )
+    const [session, setSession] = useState(null);
+
+    useEffect(() => {
+        const fetchSession = async () => {
+            const session = await getSession();
+            setSession(session);
+        };
+
+        fetchSession();
+    }, []);
+
+    return (
+        <SessionProvider session={session}>
+            {session ? <p>You are already logged in</p> : <Login />}
+        </SessionProvider>
+    );
 }
