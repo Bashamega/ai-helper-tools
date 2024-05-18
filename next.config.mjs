baaed893dea4f232b9c5345fee948f7120a869a6
Bashamega <adambashaahmednaji@gmail.com>
2024-05-18 13:30:09 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      resolveExtensions: [
-        '.mdx',
-        '.tsx',
-        '.ts',
-        '.jsx',
-        '.js',
-        '.mjs',
-        '.json',
-      ],
-    },
-  },
 }
 export default nextConfig;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,8 @@
-import { getSession } from "next-auth/react";
-import React, { useState, useEffect } from "react";
-import SessionProvider from "@/components/SessionProvider";
-import { Login } from "@/components/login";
-
-export default function App() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      const session = await getSession();
-      setSession(session);
-    };
-
-    fetchSession();
-  }, []);
-
-  return (
-    <SessionProvider session={session}>
-      {session ? <p>hey</p> : <Login />}
-    </SessionProvider>
-  );
+import React from "react";
+export default function App(){
+  return(
+    <main>
+      
+    </main>
+  )
 }
