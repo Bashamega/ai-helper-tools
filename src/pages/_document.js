2026b3cd4055748e2dd02e5469d95f329149c4c0
Bashamega <adambashaahmednaji@gmail.com>
2024-08-25 15:30:48 +0300

    patch: added banner

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+import Banner from "free-ps-banner";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -5,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="dark:bg-gray-700">
+        <Banner showButton={false}/>
         <Main />
         <NextScript />
       </body>
